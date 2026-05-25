04c81ffbe1fd6714c40164a9bf7eb7faa30a3967
Biosolver <asperant001@gmail.com>
2026-05-26 01:07:28 +0700

    Update bot.py

--- leads_bot/bot.py
+++ leads_bot/bot.py
@@ -1,20 +1,28 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.session.aiohttp import AiohttpSession
+
 from handlers import lead, notify
+from storage.database import init_db
+from analytics.stats import router as stats_router
 
 TOKEN = "YOUR_BOT_TOKEN"
 PROXY = "http://127.0.0.1:10809"
 
+
 async def main():
+    await init_db()
+
     session = AiohttpSession(proxy=PROXY)
     bot = Bot(token=TOKEN, session=session)
     dp = Dispatcher()
 
     dp.include_router(lead.router)
     dp.include_router(notify.router)
+    dp.include_router(stats_router)
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
